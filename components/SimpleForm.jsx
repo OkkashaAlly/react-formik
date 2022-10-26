88b9dc0fecd1ee7ae09eb08dd48aeb0557f9346c
OkkashaAlly <steamnature006@gmail.com>
2022-10-26 07:00:42 +0300

    COMPONENTS: SimpleForm - Managing Form State

--- components/SimpleForm.jsx
+++ components/SimpleForm.jsx
@@ -1,25 +1,55 @@
 import { useFormik } from "formik";
 
 const SimpleForm = () => {
-  const formik = useFormik({})
-  
+  const formValues = {
+    name: "Okkasha",
+    email: "",
+    channel: "",
+  };
+
+  const formik = useFormik({
+    initialValues: formValues,
+  });
+
+  console.log('Form values: ', formik.values);
+
   return (
     <div className="p-6 bg-yellow-200 rounded-md">
       <form className="flex flex-col gap-3">
         <div className="flex flex-col">
           <label htmlFor="name">Name:</label>
-          <input  className="p-3 border border-black" type="text" name="name" id=" " />
+          <input
+            className="p-3 border border-black"
+            type="text"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
         </div>
         <div className="flex flex-col">
           <label htmlFor="email">Email:</label>
-          <input  className="p-3 border border-black" type="email" name="email" id=" " />
+          <input
+            className="p-3 border border-black"
+            type="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
         </div>
         <div className="flex flex-col">
           <label htmlFor="channel">Channel:</label>
-          <input className="p-3 border border-black"  type="text" name="channel" id=" " />
+          <input
+            className="p-3 border border-black"
+            type="text"
+            name="channel"
+            onChange={formik.handleChange}
+            value={formik.values.channel}
+          />
         </div>
         <div className="">
-          <button className="p-6 bg-blue-500 "type="submit">Submit</button>
+          <button className="p-6 bg-blue-500 " type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
