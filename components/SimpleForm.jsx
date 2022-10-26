143fb823cbb79e51c34e47a0aff54faf0ab9a31f
OkkashaAlly <steamnature006@gmail.com>
2022-10-26 07:47:13 +0300

    COMPONENTS: SimpleForm - Form Validation

--- components/SimpleForm.jsx
+++ components/SimpleForm.jsx
@@ -1,21 +1,44 @@
 import { useFormik } from "formik";
 
+// ====================
+// FORM(formik)========
+const initialValues = {
+  name: "Okkasha",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = values => {
+  console.log("Form values: ", values);
+};
+
+const validate = values => {
+  // errors has to have the same properties as initialValues
+  let errors = {};
+
+  // check your conditions
+  if (!values.name) errors.name = "Required";
+  if (!values.email) errors.email = "Required";
+  if (!values.channel) errors.channel = "Required";
+
+  return errors;
+};
+
+// ========================
+// SIMPLE FORM COMPONENT //
+// ========================
 const SimpleForm = () => {
-  const formValues = {
-    name: "Okkasha",
-    email: "",
-    channel: "",
-  };
-
-  const submitHandler = values => {
-    console.log("Form values: ", values);
-  };
-
+  // Formik
   const formik = useFormik({
-    initialValues: formValues,
-    onSubmit: submitHandler,
+    initialValues,
+    onSubmit,
+    validate,
   });
 
+  console.log("Form errors", formik.errors);
+
+  ////////////////////
+  // RETURN /////////
   return (
     <div className="p-6 bg-yellow-200 rounded-md">
       <form onSubmit={formik.handleSubmit} className="flex flex-col gap-3">
@@ -28,6 +51,9 @@ const SimpleForm = () => {
             onChange={formik.handleChange}
             value={formik.values.name}
           />
+          {formik.errors.name && (
+            <span className="text-red-500">{formik.errors.name}</span>
+          )}
         </div>
         <div className="flex flex-col">
           <label htmlFor="email">Email:</label>
@@ -38,6 +64,9 @@ const SimpleForm = () => {
             onChange={formik.handleChange}
             value={formik.values.email}
           />
+          {formik.errors.email && (
+            <span className="text-red-500">{formik.errors.email}</span>
+          )}
         </div>
         <div className="flex flex-col">
           <label htmlFor="channel">Channel:</label>
@@ -48,6 +77,9 @@ const SimpleForm = () => {
             onChange={formik.handleChange}
             value={formik.values.channel}
           />
+          {formik.errors.channel && (
+            <span className="text-red-500">{formik.errors.channel}</span>
+          )}
         </div>
         <div className="">
           <button className="p-6 bg-blue-500 " type="submit">
