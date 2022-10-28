70faa8e7fff05238a247b9092f612145c4ad0254
OkkashaAlly <steamnature006@gmail.com>
2022-10-28 06:59:16 +0300

    COMPONENTS: SimpleForm - Submit validation

--- components/SimpleForm.jsx
+++ components/SimpleForm.jsx
@@ -47,155 +47,171 @@ const SimpleForm = () => {
       onSubmit={onSubmit}
       validateOnChange={false} // stops validation on each input change
       // validateOnBlur={false}  // stops validation on each element from focus
+      // validateOnMount // auto run validation on component load/mount
     >
-      <div className="p-6 bg-yellow-200 rounded-md w-7/12">
-        <Form className="flex flex-col gap-3">
-          <div className="flex flex-col">
-            <label htmlFor="name">Name:</label>
-            <Field
-              className="p-3 border border-black   "
-              type="text"
-              name="name"
-              placeholder="Enter your full name"
-            />
-            <ErrorMessage name="name" component={TextError} />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="email">Email:</label>
-            <Field
-              className="p-3 border border-black"
-              type="email"
-              name="email"
-            />
-            <ErrorMessage name="email" component={TextError} />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="channel">Channel:</label>
-            <Field
-              className="p-3 border border-black"
-              type="text"
-              name="channel"
-            />
-            <ErrorMessage name="channel" component={TextError} />
-          </div>
+      {formik => {
+        console.log("Formik properties", formik);
 
-          <div className="flex flex-col">
-            <label htmlFor="comments">Comments:</label>
-            <Field
-              className="p-3 border border-black"
-              as="textarea"
-              name="comments"
-              validate={validateComments}
-            />
-            <ErrorMessage name="comments" component={TextError} />
-          </div>
+        return (
+          <div className="p-6 bg-yellow-200 rounded-md w-7/12">
+            <Form className="flex flex-col gap-3">
+              <div className="flex flex-col">
+                <label htmlFor="name">Name:</label>
+                <Field
+                  className="p-3 border border-black   "
+                  type="text"
+                  name="name"
+                  placeholder="Enter your full name"
+                />
+                <ErrorMessage name="name" component={TextError} />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email">Email:</label>
+                <Field
+                  className="p-3 border border-black"
+                  type="email"
+                  name="email"
+                />
+                <ErrorMessage name="email" component={TextError} />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="channel">Channel:</label>
+                <Field
+                  className="p-3 border border-black"
+                  type="text"
+                  name="channel"
+                />
+                <ErrorMessage name="channel" component={TextError} />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="address">Address:</label>
-            <Field name="address">
-              {({ form }) => {
-                // console.log(form.errors);
-                return (
-                  <input type="text" className="p-3 border border-black" />
-                );
-              }}
-            </Field>
-            <ErrorMessage name="address" />
-          </div>
+              <div className="flex flex-col">
+                <label htmlFor="comments">Comments:</label>
+                <Field
+                  className="p-3 border border-black"
+                  as="textarea"
+                  name="comments"
+                  validate={validateComments}
+                />
+                <ErrorMessage name="comments" component={TextError} />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="facebook">Facebook:</label>
-            <Field
-              className="p-3 border border-black"
-              type="text"
-              name="socials.facebook"
-            />
-            <ErrorMessage name="facebook" component={TextError} />
-          </div>
+              <div className="flex flex-col">
+                <label htmlFor="address">Address:</label>
+                <Field name="address">
+                  {({ form }) => {
+                    // console.log(form.errors);
+                    return (
+                      <input type="text" className="p-3 border border-black" />
+                    );
+                  }}
+                </Field>
+                <ErrorMessage name="address" />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="twitter">twitter:</label>
-            <Field
-              className="p-3 border border-black"
-              type="text"
-              name="socials.twitter"
-            />
-            <ErrorMessage name="twitter" component={TextError} />
-          </div>
+              <div className="flex flex-col">
+                <label htmlFor="facebook">Facebook:</label>
+                <Field
+                  className="p-3 border border-black"
+                  type="text"
+                  name="socials.facebook"
+                />
+                <ErrorMessage name="facebook" component={TextError} />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="phoneNumbers[0]">Phone no. 1</label>
-            <Field
-              className="p-3 border border-black"
-              type="text"
-              name="phoneNumbers[0]"
-            />
-            <ErrorMessage name="phoneNumbers[0]" component={TextError} />
-          </div>
+              <div className="flex flex-col">
+                <label htmlFor="twitter">twitter:</label>
+                <Field
+                  className="p-3 border border-black"
+                  type="text"
+                  name="socials.twitter"
+                />
+                <ErrorMessage name="twitter" component={TextError} />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="phoneNumbers[1]">Phone no. 2</label>
-            <Field
-              className="p-3 border border-black"
-              type="text"
-              name="phoneNumbers[1]"
-            />
-            <ErrorMessage name="phoneNumbers[1]" component={TextError} />
-          </div>
+              <div className="flex flex-col">
+                <label htmlFor="phoneNumbers[0]">Phone no. 1</label>
+                <Field
+                  className="p-3 border border-black"
+                  type="text"
+                  name="phoneNumbers[0]"
+                />
+                <ErrorMessage name="phoneNumbers[0]" component={TextError} />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="phNumbers">Enter phone numbers</label>
-            <FieldArray name="phNumbers">
-              {({ push, remove, form }) => {
-                const { phNumbers } = form.values;
+              <div className="flex flex-col">
+                <label htmlFor="phoneNumbers[1]">Phone no. 2</label>
+                <Field
+                  className="p-3 border border-black"
+                  type="text"
+                  name="phoneNumbers[1]"
+                />
+                <ErrorMessage name="phoneNumbers[1]" component={TextError} />
+              </div>
 
-                return (
-                  <div className="flex flex-col gap-2">
-                    {phNumbers.map((_, i) => {
-                      let isDisabled = i > 0;
+              <div className="flex flex-col">
+                <label htmlFor="phNumbers">Enter phone numbers</label>
+                <FieldArray name="phNumbers">
+                  {({ push, remove, form }) => {
+                    const { phNumbers } = form.values;
 
-                      return (
-                        <div key={i} className="flex w-full items-center gap-3">
-                          <Field
-                            type="text"
-                            name={`phNumbers[${i}]`}
-                            className="p-3 border border-black flex-1"
-                          />
-                          <div className="h-full flex gap-1">
-                            <button
-                              type="button"
-                              onClick={_ => {
-                                remove(i);
-                              }}
-                              disabled={!isDisabled}
-                              className="p-3 border border-black bg-slate-200"
+                    return (
+                      <div className="flex flex-col gap-2">
+                        {phNumbers.map((_, i) => {
+                          let isDisabled = i > 0;
+
+                          return (
+                            <div
+                              key={i}
+                              className="flex w-full items-center gap-3"
                             >
-                              -
-                            </button>
-                            <button
-                              type="button"
-                              onClick={_ => push("")}
-                              className="p-3 border border-black bg-slate-200"
-                            >
-                              +
-                            </button>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                );
-              }}
-            </FieldArray>
-          </div>
+                              <Field
+                                type="text"
+                                name={`phNumbers[${i}]`}
+                                className="p-3 border border-black flex-1"
+                              />
+                              <div className="h-full flex gap-1">
+                                <button
+                                  type="button"
+                                  onClick={_ => {
+                                    remove(i);
+                                  }}
+                                  disabled={!isDisabled}
+                                  className="p-3 border border-black bg-slate-200"
+                                >
+                                  -
+                                </button>
+                                <button
+                                  type="button"
+                                  onClick={_ => push("")}
+                                  className="p-3 border border-black bg-slate-200"
+                                >
+                                  +
+                                </button>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    );
+                  }}
+                </FieldArray>
+              </div>
 
-          <div className="">
-            <button className="p-6 bg-blue-500 " type="submit">
-              Submit
-            </button>
+              <div className="">
+                {/* <button disabled={!formik.isValid} className="p-6 bg-blue-500 " type="submit"> */}
+                {/* <button disabled={!(formik.dirty && formik.isValid)}> */}
+                <button
+                  disabled={!formik.isValid}
+                  className="p-6 bg-blue-500 "
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </div>
+            </Form>
           </div>
-        </Form>
-      </div>
+        );
+      }}
     </Formik>
   );
 };
