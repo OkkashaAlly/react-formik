02af181d3b5e58fcc0b09a41e50756545d423b42
OkkashaAlly <steamnature006@gmail.com>
2022-10-29 06:30:53 +0300

    COMPONENTS - FormikControll - component added

---
+++ components/FormikControl.jsx
@@ -0,0 +1,25 @@
+// ================================
+// FORMIK CONTROL COMPONENT =======
+// ================================
+const FormikControl = ({ control }) => {
+  switch (control) {
+    case "input":
+      break;
+    case "textarea":
+      break;
+    case "radio":
+      break;
+    case "select":
+      break;
+    case "checkbox":
+      break;
+    case "date":
+      break;
+
+    default:
+      return null;
+  }
+};
+
+// EXPORT ====================
+export default FormikControl;
