b3436a496a8a3892285259ae92e2838915da856d
OkkashaAlly <steamnature006@gmail.com>
2022-11-03 14:14:10 +0300

    COMPONENTS: RegistrationForm - Form created

--- components/RegistrationForm.jsx
+++ components/RegistrationForm.jsx
@@ -1,80 +1,48 @@
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import {FormControl} from "./";
+import { FormControl } from "./";
 
 // ================================
 // FORMIK CONTAINER COMPONENT =====
 // ================================
 const RegistrationForm = () => {
-  const selectOptions = [
+  const mocOptions = [
     {
-      key: "Select an option",
-      value: "",
+      key: "Email",
+      value: "emailmoc",
     },
     {
-      key: "Option 1",
-      value: "option1",
-    },
-    {
-      key: "Option 2",
-      value: "option2",
-    },
-    {
-      key: "Option 3",
-      value: "option3",
-    },
-  ];
-  const radioOptions = [
-    {
-      key: "Option 1",
-      value: "radioOption1",
-    },
-    {
-      key: "Option 2",
-      value: "radioOption2",
-    },
-    {
-      key: "Option 3",
-      value: "radioOption3",
-    },
-  ];
-
-  const checkboxOptions = [
-    {
-      key: "Option 1",
-      value: "checkboxOption1",
-    },
-    {
-      key: "Option 2",
-      value: "checkboxOption2",
-    },
-    {
-      key: "Option 3",
-      value: "checkboxOption3",
+      key: "Telephone",
+      value: "telephonemoc",
     },
   ];
 
   const initialValues = {
     email: "",
-    textarea: "",
-    select: "",
-    radio: "",
-    checkbox: null,
-    date: null,
+    password: "",
+    confirmPassword: "",
+    modeOfContact: "",
+    phone: "",
   };
+
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email").required("Required"),
-    textarea: Yup.string().required("Required"),
-    select: Yup.string().required("Required"),
-    radio: Yup.string().required("Required"),
-    checkbox: Yup.array().of(Yup.string()).required("Required").nullable(),
-    date: Yup.date().required("Required").nullable(),
+    password: Yup.string().required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), ""], "Password must match")
+      .required("Required"),
+    modeOfContact: Yup.string().required("Required"),
+    phone: Yup.string().when("modeOfContact", {
+      is: "telephonemoc",
+      then: Yup.string().required("Required"),
+    }),
   });
+
   const handleSubmit = values => {
     console.log("Form values: ", values);
     // For formatting the date value
     // console.log("Form values: ", JSON.parse(JSON.stringify(values)));
-    // OR use Date Parser 
+    // OR use Date Parser
   };
   /////////////////
   // RETURN =======
@@ -85,8 +53,8 @@ const RegistrationForm = () => {
       onSubmit={handleSubmit}
     >
       {formik => (
-        <Form className="p-4 bg-gray-200 rounded-md">
-          {/* Input  */}
+        <Form className="p-4 bg-gray-200 w-[70%] rounded-md">
+          {/* email  */}
           <FormControl
             control={"input"}
             label={"Email"}
@@ -95,39 +63,38 @@ const RegistrationForm = () => {
             placeholder={"example@example.com"}
           />
 
-          {/* Textarea  */}
+          {/* password  */}
           <FormControl
-            control={"textarea"}
-            label={"Description"}
-            name={"textarea"}
+            control={"input"}
+            label={"password"}
+            name={"password"}
+            type={"password"}
+          />
+          
+          {/* confirmPassword  */}
+          <FormControl
+            control={"input"}
+            label={"confirm Password"}
+            name={"confirmPassword"}
+            type={"password"}
           />
 
-          {/* Select  */}
-          <FormControl
-            control={"select"}
-            label={"Select topic"}
-            name={"select"}
-            options={selectOptions}
-          />
-
-          {/* Radio  */}
+          {/* modeOfContact  */}
           <FormControl
             control={"radio"}
-            label={"Radio topic"}
-            name={"radio"}
-            options={radioOptions}
+            label={"mode Of Contact"}
+            name={"modeOfContact"}
+            options={mocOptions}
           />
 
-          {/* checkbox  */}
+          {/* confirmPassword  */}
           <FormControl
-            control={"checkbox"}
-            label={"checkbox ii topic"}
-            name={"checkbox"}
-            options={checkboxOptions}
+            control={"input"}
+            label={"phone"}
+            name={"phone"}
+            type={"number"}
           />
-
-          {/* date  */}
-          <FormControl control={"date"} label={"Pick a date"} name={"date"} />
+          
 
           <button className="py-2 px-3 bg-green-500 mt-3" type="submit">
             Submit
